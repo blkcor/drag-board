17c7a3b8d9d00e1fe54bf34f3d89e54e3c75a8dd
blkcor <1029129867@qq.com>
2023-03-20 17:31:42 +0800

    add favicon

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+useServerSeoMeta({
+  title: 'My Amazing Site',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://vueschool.io/img/logo/vueschool_logo_multicolor_negative.svg',
+  twitterCard: 'summary_large_image',
+})
+</script>
 <template>
   <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
     <h1 class="text-4xl text-white flex items-center mb-10">
